e55ef013033182077d122ad9c2b7b98b82790d1b
YYC <waiwaixi914@gmail.com>
2024-10-17 15:27:44 +0800

    数据库配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://your-external-database-url/:path*', // 替换为你的外部数据库URL
+            },
+        ];
+    },
+};
 
 export default nextConfig;
